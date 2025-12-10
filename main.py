e12704e24410679fdbce377629bd8254f35e2163
santrossi <santiago_rossi@live.com>
2025-12-09 23:20:13 -0300

    UPDATE: version final del programa principal

--- main.py
+++ main.py
@@ -13,24 +13,23 @@ def gestion_de_inventario():
     fn.nueva_tabla()
     while(True):
         accion = fn.menu()
-        if (accion == 1):
-            fn.registrar_producto()
-        elif (accion == 2):
-            fn.mostrar_productos()
-        elif (accion == 3):
-            fn.actualizar_cantidad_de_producto()
-        elif (accion == 4):
-            fn.eliminar_producto()
-        elif (accion == 5):
-            fn.buscar_producto()
-        elif (accion == 6):
-            print(cl.Fore.CYAN + "\n------------------------------------\n • Seleccionaste CONTROL DE STOCK •\n------------------------------------\n")
-            limite = int(input(" > Ingrese el umbral de bajo stock (por ejemplo, 20): "))
-            fn.control_stock(limite)
-        elif (accion ==7):
-            print("\nGracias por utilizar el programa.\n")
-            break
-        else:
-            print(cl.Fore.RED + "\n ERROR | Ingrese una opción numérica válida.\n")
+        match accion:
+            case 1:
+                fn.registrar_producto()
+            case 2:
+                fn.mostrar_productos()
+            case 3:
+                fn.actualizar_cantidad_de_producto()
+            case 4:
+                fn.eliminar_producto()
+            case 5:
+                fn.buscar_producto()
+            case 6:
+                fn.control_stock()
+            case 7:
+                fn.cierre_programa()
+                break
+            case _:
+                print(cl.Fore.RED + "\n ERROR | Ingrese una opción de la lista.\n")
 
 gestion_de_inventario()
