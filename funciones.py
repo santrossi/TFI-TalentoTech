eae69e99e7394fe4e0feb3f6013fc99232654b5e
santrossi <santiago_rossi@live.com>
2025-12-11 13:24:46 -0300

    ADD: Comentarios explicativos

--- funciones.py
+++ funciones.py
@@ -1,15 +1,12 @@
-#----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+    # En este archivo se encuentran las funciones del programa de gestión de inventario
+#-----------------------------------------------------------------------------------------------------------------------------------
 
-# En este archivo, se encuentran las funciones utilizadas en el código principal del programa de gestión de inventario
+import sqlite3 as sql       # Se importa el módulo SQLite3, para gestionar la base de datos con comandos SQL
+import colorama as cl       # Para incorporar color al programa, se importa Colorama
+cl.init()                   # Se inicia Colorama
 
-
-#----------------------------------------------------------------------------------------------------------------------------
-
-import sqlite3 as sql
-import colorama as cl
-cl.init()
-
-def nueva_tabla():
+def nueva_tabla():          # Esta función crea una tabla en la base de datos, si todavía no existe
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
     cursor.execute('''
@@ -25,11 +22,11 @@ def nueva_tabla():
     conexion.commit()
     conexion.close()
 
-def ingresar_nombre():
+def ingresar_nombre():      # Esta función le pide el ingreso de un nombre al usuario, para su registro en la DB
     while True:
         try:
-            ingreso = input(f" > Ingrese el nombre del producto: ").strip().capitalize()
-            if (len(ingreso) < 1):
+            ingreso = input(f" > Ingrese el nombre del producto: ").strip().capitalize()    # Se corrige el formato
+            if (len(ingreso) < 1):                                                          # Detecta si está vacío, y avisa el error
                 raise ValueError(f"El campo no puede quedar vacío.\n Intente nuevamente.\n")
             else:
                 break
@@ -38,41 +35,41 @@ def ingresar_nombre():
             continue
     return ingreso
 
-def ingresar_dato_opcional(dato):
+def ingresar_dato_opcional(dato):       # Esta función pide el ingreso de un dato *opcional* al usuario; éste puede no ingresar nada
     ingreso = input(f" > Ingrese {dato} del producto (opcional): ").strip().capitalize()
     return ingreso
 
-def ingresar_numero(detalle):
+def ingresar_numero(detalle):           # Esta función pide el ingreso de un número. Con el parámetro "detalle" se personaliza el mensaje
     while True:
         try:
             ingreso = input(f" > Ingrese {detalle}: ").strip()
-            if ingreso == "":
+            if ingreso == "":           # Detecta si está vacío, y avisa al usuario
                 raise ValueError("Debe ingresar un número. (ej: 10 | 500 | 1000)")
-            elif not ingreso.isdigit():
+            elif not ingreso.isdigit(): # Si detecta texto, devuelve error y avisa al usuario.
                 raise ValueError("Ingrese únicamente números enteros, sin comas ni puntos. (ej: 10 | 500 | 1000)")
-            cantidad = int(ingreso)
+            cantidad = int(ingreso)     # Convierte el string en número entero
             return cantidad
         except ValueError as error:
             print(cl.Fore.RED + f"\n ERROR | {error}\n" + cl.Fore.GREEN)
             continue
 
-def ingresar_precio():
+def ingresar_precio():                  # Esta función pide el ingreso de un precio
     while True:
         try:
-            ingreso = input(" > Ingrese el precio del producto: $").strip()
-            if ingreso == "":
+            ingreso = input(" > Ingrese el precio del producto: $").strip() # Se eliminan espacios
+            if ingreso == "":                                               # Comprueba si se ingresó un precio
                 raise ValueError("Debe ingresar un número. (ej: 10 | 10.50 | 10,50)")
-            ingreso = ingreso.replace(",", ".")
-            precio = float(ingreso)
-            if precio < 0:
+            ingreso = ingreso.replace(",", ".")                             # Si escriben con "," se reemplaza por un "." para mantener sintaxis correcta
+            precio = float(ingreso)                                         # Se convierte en número decimal
+            if precio < 0:                                                  # Se comprueba que no se haya ingresado un "0" como valor
                 raise ValueError("El precio no puede ser negativo.\n Intente nuevamente.")
             return precio
         except ValueError:
             print(cl.Fore.RED + "\n ERROR | Ingrese un número válido (ej: 10 | 10.50 | 10,50)\n" + cl.Fore.GREEN)
             continue
 
-def menu():
-    print(cl.Fore.MAGENTA +"\n----------------------------------\n • MENU | Gestión de Inventario •\n----------------------------------\n")
+def menu():                             # Esta función imprime el menú con las distintas acciones realizables
+    print(cl.Fore.MAGENTA +"\n----------------------------------\n • MENU | Gestión de Inventario •\n----------------------------------\n") # Encabezado
     print(cl.Fore.WHITE + "(1) - Registrar un producto")
     print("(2) - Ver todos los productos")
     print("(3) - Actualizar cantidad de un producto")
@@ -80,85 +77,83 @@ def menu():
     print("(5) - Buscar un producto")
     print("(6) - Control de stock")
     print("(7) - Salir\n")
-    accion = ingresar_numero(cl.Fore.WHITE + "el número de la acción que desea realizar")
+    accion = ingresar_numero(cl.Fore.WHITE + "el número de la acción que desea realizar")   # Se pide la acción a realizar
     return accion
 
-def registrar_producto():
-    print(cl.Fore.GREEN + "\n-----------------------------------------\n • Seleccionaste REGISTRAR UN PRODUCTO •\n-----------------------------------------\n")
-    nombre_prod = ingresar_nombre()
+def registrar_producto():               # Esta función añade un nuevo registro a la base de datos (un producto)
+    print(cl.Fore.GREEN + "\n-----------------------------------------\n • Seleccionaste REGISTRAR UN PRODUCTO •\n-----------------------------------------\n") # Encabezado
+    nombre_prod = ingresar_nombre()     # Se recopilan los datos a almacenar, llamando a las funciones que comprueban el ingreso de datos válidos
     descripcion_prod = ingresar_dato_opcional("descripción")
     cantidad_prod = ingresar_numero("la cantidad del producto")
     precio_prod = ingresar_precio()
     categoria_prod = ingresar_dato_opcional("categoría")
-
-    conexion = sql.connect("inventario.db")
+    conexion = sql.connect("inventario.db")    # Se inicia una conexión SQL con la base de datos, 
     cursor = conexion.cursor()
+    # Se añade el nuevo registro (mediante consulta parametrizada, evitando inyecciones)
     cursor.execute('''
         INSERT INTO productos (nombre, descripcion, cantidad, precio, categoria)
         VALUES (?, ?, ?, ?, ?)
     ''', (nombre_prod,descripcion_prod,cantidad_prod,precio_prod,categoria_prod))
-    conexion.commit()
+    conexion.commit()       # Se cierra la conexión
     conexion.close()
+    print("\n-----------------------\n - Producto agregado -\n-----------------------\n")   # Mensaje de confirmación para el usuario
 
-    print("\n-----------------------\n - Producto agregado -\n-----------------------\n")
-
-def mostrar_productos():
-    print(cl.Fore.YELLOW + "\n-------------------------------------------\n • Seleccionaste VER TODOS LOS PRODUCTOS •\n-------------------------------------------\n")
+def mostrar_productos():                # Esta función muestra en una lista todos los registros existentes en la tabla (los productos del inventario)
+    print(cl.Fore.YELLOW + "\n-------------------------------------------\n • Seleccionaste VER TODOS LOS PRODUCTOS •\n-------------------------------------------\n") # Encabezado
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
     cursor.execute('SELECT * FROM productos')
     productos = cursor.fetchall()
-    if (len(productos)>0):
+    if (len(productos)>0):      # Si hay registros, los imprime en una lista
         for producto in productos:
             print(f"ID: {producto[0]} | Nombre: {producto[1]} | Descripción: {producto[2]} | Cantidad: {producto[3]} | Precio: ${producto[4]} | Categoría: {producto[5]}")
-    else:
+    else:                       # Si no hay registros, avisa al usuario
         print("No hay ningún producto registrado.")
     conexion.commit()
     conexion.close()
 
-def actualizar_cantidad_de_producto():
-    print(cl.Fore.GREEN + "\n------------------------------------------------------\n • Seleccionaste ACTUALIZAR CANTIDAD DE UN PRODUCTO •\n------------------------------------------------------\n")
-    id_producto = ingresar_numero(cl.Fore.GREEN + "el ID del producto que desea actualizar")
+def actualizar_cantidad_de_producto():  # Esta función actualiza un registro de la tabla (la cantidad de un producto) mediante su ID
+    print(cl.Fore.GREEN + "\n------------------------------------------------------\n • Seleccionaste ACTUALIZAR CANTIDAD DE UN PRODUCTO •\n------------------------------------------------------\n") # Encabezado 
+    id_producto = ingresar_numero(cl.Fore.GREEN + "el ID del producto que desea actualizar")        # Se pide el ID y la cantidad a actualizar
     nueva_cantidad = ingresar_numero(cl.Fore.GREEN + "la cantidad actualizada")
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
     cursor.execute('UPDATE productos SET cantidad = ? WHERE id = ?', (nueva_cantidad, id_producto))
     conexion.commit()
     conexion.close()
-    print (cl.Fore.GREEN + "\nCantidad actualizada exitosamente.\n")
+    print (cl.Fore.GREEN + "\nCantidad actualizada exitosamente.\n")        # Se printea un mensaje de confirmación al usuario
 
-def eliminar_producto():
-    print(cl.Fore.RED + "\n----------------------------------------\n • Seleccionaste ELIMINAR UN PRODUCTO •\n----------------------------------------\n")
+def eliminar_producto():                # Esta función elimina un registro de la tabla (un producto del inventario) mediante su ID
+    print(cl.Fore.RED + "\n----------------------------------------\n • Seleccionaste ELIMINAR UN PRODUCTO •\n----------------------------------------\n") # Encabezado
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
-    id_producto = ingresar_numero(cl.Fore.RED + "el ID del producto a eliminar")
+    id_producto = ingresar_numero(cl.Fore.RED + "el ID del producto a eliminar")    # Se pide el ID del registro a eliminar
     cursor.execute('DELETE FROM productos WHERE id = ?', (id_producto,))
     conexion.commit()
-    print("\n------------------------\n - Producto eliminado -\n------------------------\n")
-    conexion.commit()
     conexion.close()
+    print("\n------------------------\n - Producto eliminado -\n------------------------\n")    # Se printea un mensaje de confirmación al usuario
 
-def buscar_producto():
-    print(cl.Fore.YELLOW + "\n--------------------------------------\n • Seleccionaste BUSCAR UN PRODUCTO •\n--------------------------------------\n")
-    id_producto = ingresar_numero(cl.Fore.YELLOW + "el ID del producto que desea buscar")
+def buscar_producto():                  # Esta función busca un registro (un producto del inventario) mediante su ID
+    print(cl.Fore.YELLOW + "\n--------------------------------------\n • Seleccionaste BUSCAR UN PRODUCTO •\n--------------------------------------\n") # Encabezado
+    id_producto = ingresar_numero(cl.Fore.YELLOW + "el ID del producto que desea buscar")   # Se pide el ID del registro a buscar
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
     cursor.execute('SELECT * FROM productos WHERE id = ?', (id_producto,))
     productos = cursor.fetchall()
-    for producto in productos:
+    for producto in productos:          
         print(f"\nID: {producto[0]} | Nombre: {producto[1]} | Descripción: {producto[2]} | Cantidad: {producto[3]} | Precio: ${producto[4]} | Categoría: {producto[5]}\n")
     conexion.commit()
     conexion.close()
 
-def control_stock():
-    print(cl.Fore.CYAN + "\n------------------------------------\n • Seleccionaste CONTROL DE STOCK •\n------------------------------------\n")
-    limite = ingresar_numero(cl.Fore.CYAN + "el umbral de bajo stock (por ejemplo, 20)")
+def control_stock():                    # Esta función muestra todos los registros de la tabla con stock menor al indicado por el usuario
+    print(cl.Fore.CYAN + "\n------------------------------------\n • Seleccionaste CONTROL DE STOCK •\n------------------------------------\n") # Encabezado
+    limite = ingresar_numero(cl.Fore.CYAN + "el umbral de bajo stock (por ejemplo, 20)")    # Se pide el número que se considera bajo stock
     conexion = sql.connect("inventario.db")
     cursor = conexion.cursor()
-    cursor.execute("SELECT * FROM productos WHERE cantidad <= ?", (limite,))
+    cursor.execute("SELECT * FROM productos WHERE cantidad <= ?", (limite,))                # Se buscan los registros con menor stock al indicado
     productos = cursor.fetchall()
-    print("\n--------------------------------------------------------\n • PRODUCTOS CON STOCK POR DEBAJO DEL UMBRAL INDICADO •\n--------------------------------------------------------\n")
-    if (len(productos)>0):
+    print("\n--------------------------------------------------------\n • PRODUCTOS CON STOCK POR DEBAJO DEL UMBRAL INDICADO •\n--------------------------------------------------------\n") # Encabezado de lista
+    if (len(productos)>0):              # Se printean los registros si es que existen
         for producto in productos:
             print(f"ID: {producto[0]} | Nombre: {producto[1]} | Descripción: {producto[2]} | Cantidad: {producto[3]} | Precio: ${producto[4]} | Categoría: {producto[5]}")
     else:
@@ -166,5 +161,5 @@ def control_stock():
     conexion.commit()
     conexion.close()
 
-def cierre_programa():
+def cierre_programa():                  # Esta función muestra un mensaje de despedida, y resetea el color de la consola al predeterminado
     print(cl.Fore.YELLOW + "\nGracias por utilizar el programa.\n" + cl.Style.RESET_ALL)
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-#----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+    # En este archivo se encuentra el código principal del programa de gestión de inventario
+#-----------------------------------------------------------------------------------------------------------------------------------
 
-# En este archivo se encuentra el código principal del programa de gestión de inventario
+import funciones as fn      # Se importa el archivo "funciones.py", que hace de módulo facilitador a las funciones del programa  
+import sqlite3 as sql       # Para gestionar la base de datos mediante el uso de comandos SQL, se importa el módulo SQLite3
+import colorama as cl       # Se importa Colorama para incorporar color al programa, a fin de orientar al usuario
 
-
-#----------------------------------------------------------------------------------------------------------------------------
-import funciones as fn
-import sqlite3 as sql
-import colorama as cl
-cl.init()
-
-def gestion_de_inventario():
-    fn.nueva_tabla()
-    while(True):
+def gestion_de_inventario():   # Código principal del programa
+    fn.nueva_tabla()           # Se crea la tabla PRODUCTOS en la base de datos, si es que no fue creada previamente
+    while(True):               # Espera que el usuario elija una de las funciones del programa, y llama a la función correspondiente
         accion = fn.menu()
         match accion:
             case 1:
@@ -28,8 +25,8 @@ def gestion_de_inventario():
                 fn.control_stock()
             case 7:
                 fn.cierre_programa()
-                break
-            case _:
+                break           # Termina el loop "while (True)" y cierra el programa
+            case _:             # Si el usuario ingresa un caracter que no es un número del 1 al 7, se printea un ERROR avisando al usuario
                 print(cl.Fore.RED + "\n ERROR | Ingrese una opción de la lista.\n")
 
-gestion_de_inventario()
+gestion_de_inventario()         # Llamado a la función principal "gestion_de_inventario", que inicia el programa
